897aa40dbc7b5a7232c44b29fc38c723a569074a
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2019-12-19 22:50:18 +0100

    Menu avec meilleur UX et début des RegEx

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
 $(function(){
-
-})
+  // RegEx
+  // \u00C0-\u00FF = All accented characters (ASCII 192 -> 255)
+  var regExName = /^[a-zA-Z \u00C0-\u00FF]{1,100}$/;
+  // Not finished yet
+  var regExMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@$/;
+  // Need automatic scroll bottom
+  $('body').keydown(function(){
+    if ($('#secondSlide').is(':visible')){
+      if ($('#thirdSlide').is(':visible')){
+        if ($('#fourthSlide').is(':visible')){
+          if ($('#fourthSlide').is(':visible')){
+            $('#submitButton').show();
+            $('#submitButton').addClass('d-block');
+          } else {
+            $('#fourthSlide').slideDown('slow');
+          }
+        } else {
+          $('#fourthSlide').slideDown('slow');
+        }
+      } else {
+        $('#thirdSlide').slideDown('slow');
+      }
+    } else {
+      $('#secondSlide').slideDown('slow');
+    }
+  });
+});
--- index.php
+++ index.php
@@ -11,81 +11,93 @@
   <div class="card m-auto p-1 bg-success shadow-lg">
     <div class="card-body">
       <div>
-        <form autocomplete="on">
+        <form action="index.php" method="get" autocomplete="on">
           <!-- First slide -->
           <div class="form-group">
             <label for="lastname">Nom</label>
-            <input type="text" class="form-control" id="lastname" placeholder="Doe">
+            <input type="text" class="form-control" id="lastname" name="lastName" placeholder="Doe">
           </div>
           <div class="form-group">
             <label for="firstname">Prénom</label>
-            <input type="text" class="form-control" id="firstname" placeholder="John">
+            <input type="text" class="form-control" id="firstname" name="firstName" placeholder="John">
           </div>
           <div class="form-group">
             <label for="birthday">Date de naissance</label>
-            <input type="date" class="form-control" id="birthday" placeholder="19/08/1075">
+            // The date input type is not supported in all browsers. Please be sure to test, and consider using a polyfill.
+            <input type="date" class="form-control" id="birthday" name="birthday">
           </div>
           <!-- Second slide -->
-          <div class="form-group">
-            <label for="country">Pays</label>
-            <input type="text" class="form-control" id="country" placeholder="France">
-          </div>
-          <div class="form-group">
-            <label for="nationality">Nationalité</label>
-            <input type="text" class="form-control" id="nationality" placeholder="Français">
-          </div>
-          <div class="form-group">
-            <label for="adress">Adresse</label>
-            <input type="text" class="form-control" id="adress" placeholder="73 Rue de Verdun...">
+          <div id="secondSlide">
+            <div class="form-group">
+              <label for="country">Pays</label>
+              <input type="text" class="form-control" id="country" name="country" placeholder="France">
+            </div>
+            <div class="form-group">
+              <label for="nationality">Nationalité</label>
+              <input type="text" class="form-control" id="nationality" name="nationality" placeholder="Français">
+            </div>
+            <div class="form-group">
+              <label for="adress">Adresse</label>
+              <input type="text" class="form-control" id="adress" name="adress" placeholder="73 Rue de Verdun...">
+            </div>
           </div>
           <!-- Third slide -->
-          <div class="form-group">
-            <label for="email">Mail</label>
-            <input type="email" class="form-control" id="email" placeholder="john.doe@mail.com">
-          </div>
-          <div class="form-group">
-            <label for="phone">Téléphone</label>
-            <input type="text" class="form-control" id="phone" placeholder="0X/11/22/33/44">
+          <div id="thirdSlide">
+            <div class="form-group">
+              <label for="email">Mail</label>
+              <input type="email" class="form-control" id="email" name="email" placeholder="john.doe@mail.com">
+            </div>
+            <div class="form-group">
+              <label for="phone">Téléphone</label>
+              <input type="text" class="form-control" id="phone" name="phoneNumber" placeholder="0X/11/22/33/44">
+            </div>
           </div>
           <!-- Fourth Slide -->
-          <div class="form-group">
-            <label for="degree">Diplôme</label>
-            <input list="levels" name="degree" id="degree" placeholder="Diplômes">
-            <datalist id="levels">
-              <option value="Sans" />
-              <option value="Bac" />
-              <option value="Bac +2" />
-              <option value="Bac +3" />
-              <option value="Supérieur" />
-            </datalist>
-          </div>
-          <div class="form-group">
-            <label for="poleEmploi">Pôle Emploi</label>
-            <input type="text" class="form-control" id="poleEmploi" placeholder="1234567A">
-          </div>
-          <div class="form-group">
-            <label for="palmes">Palmes</label>
-            <input id="palmes" type="range" class="custom-range" min="0" max="6" step="1">
-          </div>
-          <div class="form-group">
-            <label for="codecademy">CodeCademy</label>
-            <input type="url" class="form-control" id="codecademy" placeholder="codecademy.com/user">
+          <div id="fourthSlide">
+            <div class="form-group">
+              <label for="degree">Diplôme</label>
+              <input list="levels" name="degree" id="degree" placeholder="Diplômes">
+              <datalist id="levels">
+                <option value="Sans">
+                <option value="Bac">
+                <option value="Bac +2">
+                <option value="Bac +3">
+                <option value="Supérieur">
+              </datalist>
+            </div>
+            <div class="form-group">
+              <label for="poleEmploi">Pôle Emploi</label>
+              <input type="text" class="form-control" id="poleEmploi" name="poleEmploiLogin" placeholder="1234567A">
+            </div>
+            <div class="form-group">
+              <label for="palmes">Palmes</label>
+              <input id="palmes" type="range" name="numberPalmes" class="custom-range" min="0" max="6" step="1">
+            </div>
+            <div class="form-group">
+              <label for="codecademy">CodeCademy</label>
+              <input type="url" class="form-control" name="codecademyURL" id="codecademy" placeholder="codecademy.com/user">
+            </div>
           </div>
           <!-- Fifth slide -->
-          <div class="form-group">
-            <label for="superhero">Superhero</label>
-            <textarea id="superhero" name="superhero" rows="5" cols="30" placeholder="Si j'étais un superhero, je serais ..."></textarea>
+          <div id="fifthSlide">
+            <div class="form-group">
+              <label for="superhero">Superhero</label>
+              <textarea id="superhero" name="superheroStory" rows="5" cols="30" placeholder="Si j'étais un superhero, je serais ..."></textarea>
+            </div>
+            <div class="form-group">
+              <label for="hackStory">Hack Story</label>
+              <textarea id="hackStory" name="hackStory" rows="5" cols="30" placeholder="Un jour, j'ai piraté ..."></textarea>
+            </div>
+            <div class="form-group d-flex">
+              <label for="experienceYes">Expérience</label>
+              <input class="my-auto mx-2" type="radio" id="experienceYes" name="experienceRadio">
+              <label for="experienceYes">OUI</label>
+              <label class="sr-only" for="experienceNo">Expérience</label>
+              <input class="my-auto mx-2" type="radio" id="experienceNo" name="experienceRadio">
+              <label for="experienceNo">NON</label>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="hackStory">Hack Story</label>
-            <textarea id="hackStory" name="hackStory" rows="5" cols="30" placeholder="Un jour, j'ai piraté ..."></textarea>
-          </div>
-          <div class="form-group">
-            <label for="experienceYes">Expérience</label>
-            <input type="radio" id="experienceYes" name="customRadio">OUI
-            <input type="radio" id="experienceNo" name="customRadio">NON
-          </div>
-          <input type="submit" name="submitButton" value="Continuer">
+          <input class="m-auto" id="submitButton" type="submit" value="Continuer">
         </form>
       </div>
     </div>
